8a4f6e71fe5b7a9d4192176922ce61aa94464bfd
HabeebPc <habeebrahmanpc00@gmail.com>
2023-10-02 09:52:11 +0530

    oct 2

--- user_auth/admin.py
+++ user_auth/admin.py
@@ -4,6 +4,32 @@ from user_auth.models import User
 
 
 class UserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            ('Personal info'),
+            {
+                'fields': (
+                    'name',
+                    'email',
+                    'mobile',
+                    'premium_user'
+                )
+            }
+        ),
+
+        (
+            ('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'user_type'
+                )
+            }
+        ),
+        (('Important dates'), {'fields': ('last_login',)}))
     list_display = ('id', 'name', 'email', 'user_type', 'premium_user')
 
 
